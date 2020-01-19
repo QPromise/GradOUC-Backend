725c13496b0253e2b5708d392db73c6af1db1ab1
Qpromise <815221919@qq.com>
2020-01-19 17:04:57 +0800

    update score.py

--- OUC/core/score.py
+++ OUC/core/score.py
@@ -72,50 +72,54 @@ def main(username = '',password = ''):
         """
         计划内的课程
         """
-        planned_table = pd.read_html(course_page.text)[0]
-        planned_table = pd.DataFrame(planned_table)
-        planned_table = planned_table.fillna("")
-        # print(len(planned_table))
-        # print(planned_table[1:2].values)
-        planned_courses = []
-        scores = []
-        credits = []
-        if len(planned_table) != 0:
-            for i in range(len(planned_table.values)):
-                planned_course = {"name": "", "type": "", "credit": None,
-                                  "teacher": "", "score":None,"selected":False,"disabled":True}
-                planned_course["name"] = planned_table[i:i + 1].values[0][2]
-                planned_course["type"] = planned_table[i:i + 1].values[0][3]
-                planned_course["credit"] = planned_table[i:i + 1].values[0][4]
-                planned_course["teacher"] = planned_table[i:i + 1].values[0][7]
-                process = planned_table[i:i + 1].values[0][8]
-                # 成绩出了
-                if re.search(r"(\d+)",process):
-                    score = float(process.split()[2])
-                    # 如果成绩大于70才计算Mean
-                    if score >= 70:
-                        scores.append(score)
-                        credits.append(planned_course["credit"])
-                        planned_course["selected"] = True
-                        planned_course["disabled"] = False
-                # 成绩没出
-                else:
-                    # 选了的课
-                    if process.find("未选") == -1:
-                        score = "未出"
+        try:
+            planned_table = pd.read_html(course_page.text)[0]
+            planned_table = pd.DataFrame(planned_table)
+            planned_table = planned_table.fillna("")
+            # print(len(planned_table))
+            # print(planned_table[1:2].values)
+            planned_courses = []
+            scores = []
+            credits = []
+            if len(planned_table) != 0:
+                for i in range(len(planned_table.values)):
+                    planned_course = {"name": "", "type": "", "credit": None,
+                                      "teacher": "", "score":None,"selected":False,"disabled":True}
+                    planned_course["name"] = planned_table[i:i + 1].values[0][2]
+                    planned_course["type"] = planned_table[i:i + 1].values[0][3]
+                    planned_course["credit"] = planned_table[i:i + 1].values[0][4]
+                    planned_course["teacher"] = planned_table[i:i + 1].values[0][7]
+                    process = planned_table[i:i + 1].values[0][8]
+                    # 成绩出了
+                    if re.search(r"(\d+)",process):
+                        score = float(process.split()[2])
+                        # 如果成绩大于70才计算Mean
+                        if score >= 70:
+                            scores.append(score)
+                            credits.append(planned_course["credit"])
+                            planned_course["selected"] = True
+                            planned_course["disabled"] = False
+                    # 成绩没出
                     else:
-                        score = "未选"
-                planned_course["score"] = score
-                print(score,process)
-                planned_courses.append(planned_course)
-            if scores !=[] and credits !=[]:
-                res["mean"] = calculate_score(scores,credits)
-                print(calculate_score(scores,credits))
-            else:
-                res["mean"] = 0
-                print("当前没有可计算的课程")
-            res['courses'] = planned_courses
-            res['have_class'] = 1
+                        # 选了的课
+                        if process.find("未选") == -1:
+                            score = "未出"
+                        else:
+                            score = "未选"
+                    planned_course["score"] = score
+                    print(score,process)
+                    planned_courses.append(planned_course)
+                if scores !=[] and credits !=[]:
+                    res["mean"] = calculate_score(scores,credits)
+                    print(calculate_score(scores,credits))
+                else:
+                    res["mean"] = 0
+                    print("当前没有可计算的课程")
+                res['courses'] = planned_courses
+                res['have_class'] = 1
+        except:
+            print('error')
+            return res
         return res
 if __name__ == '__main__':
     print(main("21190211105","tel1314"))
