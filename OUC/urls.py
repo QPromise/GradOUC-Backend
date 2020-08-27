fee9502f60abc65082472ea05a6e060eaa61e1e1
Qpromise <815221919@qq.com>
2020-08-27 16:27:27 +0800

    optimize shenpi

--- OUC/urls.py
+++ OUC/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     url(r'^get_schoolNews/$', views.get_schoolNews, name='get_schoolNews'),
     url(r'^get_schoolNewsDetail/$', views.get_schoolNewsDetail, name='get_schoolNewsDetail'),
     url(r'^get_swiper/$', views.get_swiper, name='get_swiper'),
-    url(r'^shenpi_index/', views.shenpi_index, name='shenpi_index'),
-    url(r'^shenpi_get_param/', views.shenpi_get_param, name='shenpi_get_param')
+    url(r'^shenpi_index/$', views.shenpi_index, name='shenpi_index'),
+    url(r'^shenpi_submit/', views.shenpi_submit, name='shenpi_submit')
 ]
