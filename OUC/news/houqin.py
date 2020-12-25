a8e76eeca9697bf2bf5d6e1c58211e0d2a7ddf74
Qpromise <815221919@qq.com>
2020-12-25 11:20:42 +0800

    fix school news bug

--- OUC/news/houqin.py
+++ OUC/news/houqin.py
@@ -11,6 +11,11 @@ import html2text
 import urllib.request
 from bs4 import BeautifulSoup
 import re
+import time
+
+from OUC import log
+
+logger = log.logger
 
 """
 网站新闻爬取
@@ -38,7 +43,7 @@ def get_news(page):
     pages_count = int(news_soup.find("em", {"class": "all_pages"}).text)
     res = {}
     for elem_news, elem_date in zip(temp_news, temp_dates):
-        news = {}
+        news = dict()
         news["id"] = elem_news.find("a")["href"]
         news["title"] = str(elem_news.find("a")["title"]).strip()
         news["date"] = str(elem_date.text).strip()
@@ -64,57 +69,60 @@ def get_newsDeatil(id):
     html = rsp.read().decode('utf-8', 'ignore')
     # 使用BeautifulSoup模块解析变量中的web内容
     news_soup = BeautifulSoup(html, 'lxml')
-    # print(news_soup)
+    try:
+        title = news_soup.find("h1", {"class": "arti_title"}).text.strip()
+        news_time = news_soup.find("span", {"class": "arti_update"}).text.strip()
+        content = str(news_soup.find("div", {"class": "wp_articlecontent"}))
+        image_url = re.findall(r'<img[^>]*src="([^"]*)"', content)
+        link_url = re.findall(r'<a[^>]*href="([^"]*)"', content)
+        pdf_url = re.findall(r'<div[^>]*pdfsrc="([^"]*)"', content)
+        # print(pdf_url)
+        pdf_div = re.findall(r'<div[^>]*pdfsrc=.*"></div>', content)
+        # print(image_url)
+        # print(link_url)
+        # print(pdf_div) # class="wp_pdf_player"
+        # 如果页面中有图片链接 则替换为相对路径
+        if len(image_url) != 0:
+            for url in image_url:
+                if url == '/_ueditor/themes/default/images/icon_doc.gif':
+                    content = content.replace('<img src="/_ueditor/themes/default/images/icon_doc.gif"/>', '')
+                else:
+                    content = content.replace(url, 'http://hqbzc.ouc.edu.cn' + url)
+        else:
+            pass
+        # 如果页面中有地址链接 则替换为相对路径
+        if len(link_url) != 0:
+            for url in link_url:
+                if url.find('http') == -1:
+                    content = content.replace(url, 'http://hqbzc.ouc.edu.cn' + url)
 
-    title = news_soup.find("h1", {"class": "arti_title"}).text.strip()
-    time = news_soup.find("span", {"class": "arti_update"}).text.strip()
-    content = str(news_soup.find("div", {"class": "wp_articlecontent"}))
-    image_url = re.findall(r'<img[^>]*src="([^"]*)"', content)
-    link_url = re.findall(r'<a[^>]*href="([^"]*)"', content)
-    pdf_url = re.findall(r'<div[^>]*pdfsrc="([^"]*)"', content)
-    # print(pdf_url)
-    pdf_div = re.findall(r'<div[^>]*pdfsrc=.*"></div>', content)
-    # print(image_url)
-    # print(link_url)
-    # print(pdf_div) # class="wp_pdf_player"
-    # 如果页面中有图片链接 则替换为相对路径
-    if image_url != []:
-        for url in image_url:
-            if url == '/_ueditor/themes/default/images/icon_doc.gif':
-                content = content.replace('<img src="/_ueditor/themes/default/images/icon_doc.gif"/>', '')
-            else:
-                content = content.replace(url, 'http://hqbzc.ouc.edu.cn' + url)
-    else:
-        pass
-    # 如果页面中有地址链接 则替换为相对路径
-    if link_url != []:
-        for url in link_url:
-            if url.find('http') == -1:
-                content = content.replace(url, 'http://hqbzc.ouc.edu.cn' + url)
-
-            else:
-                pass
-    else:
-        pass
-    # pdf路径替换
-    if pdf_div != []:
-        for i, pdf in enumerate(pdf_div):
-            if pdf.find('http') == -1:
-                pdf_url = re.findall(r'<div[^>]*pdfsrc="([^"]*)"', pdf)
-                if pdf_url != []:
-                    content = content.replace(pdf, '<a href = "http://hqbzc.ouc.edu.cn' + pdf_url[0] + '">附件' + str(
-                        i + 1) + '地址，点击复制去浏览器下载</a>')
                 else:
                     pass
-            else:
-                pass
-    else:
-        pass
-    # print(content)
-    text_maker = html2text.HTML2Text()
-    text_maker.bypass_tables = True
-    text_maker.ignore_images = False
-    res = {"title": title, "time": time, "content": text_maker.handle(content)}
+        else:
+            pass
+        # pdf路径替换
+        if len(pdf_div) != 0:
+            for i, pdf in enumerate(pdf_div):
+                if pdf.find('http') == -1:
+                    pdf_url = re.findall(r'<div[^>]*pdfsrc="([^"]*)"', pdf)
+                    if pdf_url != []:
+                        content = content.replace(pdf, '<a href = "http://hqbzc.ouc.edu.cn' + pdf_url[0] + '">附件' + str(
+                            i + 1) + '地址，点击复制去浏览器下载</a>')
+                    else:
+                        pass
+                else:
+                    pass
+        else:
+            pass
+        # print(content)
+        text_maker = html2text.HTML2Text()
+        text_maker.bypass_tables = True
+        text_maker.ignore_images = False
+        res = {"title": title, "time": news_time, "content": text_maker.handle(content)}
+    except Exception as e:
+        logger.error("[news_id]: %s [Exception]: %s" % (id, e))
+        res = {"title": "错误提示", "time": "访问时间：" + time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()),
+               "content": "内容无法显示，请去官网查看"}
     return json.dumps(res)
 
 
