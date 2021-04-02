ddd20d939201ad6b70c4e922e817f6187ad45ea0
Qpromise <815221919@qq.com>
2021-04-02 13:17:23 +0800

    addd proxy

--- OUC/core/package/login.py
+++ OUC/core/package/login.py
@@ -108,7 +108,8 @@ class Login(object):
             # 创建一个回话
             session = requests.Session()
             session.verify = False
-            # session.proxies = proxy.ProxyIP.get_ip()
+            session.keep_alive = False
+            session.proxies = proxy.ProxyIP.get_ip()
             # 获得登录页面
             response = session.get(login_url, headers=headers, timeout=8)
             # print(response)
@@ -130,7 +131,7 @@ class Login(object):
             return {"message": "fault"}
         # 提交登录表单
         try:
-            time.sleep(1)
+            # time.sleep(1)
             post_form = session.post(url=login_url, headers=headers, timeout=8, data=values)
             home_page = session.get(url=home_url, headers=headers, timeout=8)
 
